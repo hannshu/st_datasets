8e51b73152b0349344f8afa7625276adcacaec16
17792933795 <3529967102@qq.com>
2023-02-13 23:59:22 +0800

    update dataset path

--- dataset.py
+++ dataset.py
@@ -1,9 +1,22 @@
 import scanpy as sc
-import squidpy as sq
 import pandas as pd
+import time
 import os
 
-from utils import build_adata
+from .utils import build_adata
+
+
+def get_data(dataset_func=None, top_genes=3000, **args):
+    start_time = time.time()
+    assert (dataset_func), '>>> ERROR: You must appoint a function!'
+
+    adata, n_cluster, dataset_details = dataset_func(**args)
+    sc.pp.highly_variable_genes(adata, flavor="seurat_v3", n_top_genes=top_genes)
+    sc.pp.normalize_total(adata, target_sum=1e4)
+    sc.pp.log1p(adata)
+    print('>>> INFO: dataset name: {}, size: ({}, {}), cluster: {}.({:.3f}s)'.format(dataset_details, adata.X.shape[0], adata.X.shape[1], n_cluster, time.time() - start_time))
+
+    return adata, n_cluster
 
 
 def get_dlpfc_data(id, path=None):
@@ -15,7 +28,7 @@ def get_dlpfc_data(id, path=None):
         section_id = id
 
     if (not path):
-        path = os.path.join('DLPFC', section_id)
+        path = os.path.join('spatial_dataset', 'DLPFC', section_id)
     adata = sc.read_visium(path=path)
     adata.var_names_make_unique()
     Ann_df = pd.read_csv(os.path.join(path, 'ground_truth.txt'), 
@@ -28,7 +41,7 @@ def get_dlpfc_data(id, path=None):
 
 def get_human_breast_cancer_data(path=None):
     if (not path):
-        path = 'human_breast_cancer/'
+        path = 'spatial_dataset/human_breast_cancer/'
     adata = sc.read_visium(path=path)
     adata.var_names_make_unique()
     Ann_df = pd.read_csv(os.path.join(path, 'metadata.csv'), 
@@ -41,7 +54,7 @@ def get_human_breast_cancer_data(path=None):
 
 def get_mouse_brain_cerebellum_data(path=None):
     if (not path):
-        path = 'mouse_brain_cerebellum/'
+        path = 'spatial_dataset/mouse_brain_cerebellum/'
     adata = build_adata(spot_scale=1., 
                         root_path=path, 
                         name='mouse_brain_cerebellum')
@@ -54,7 +67,7 @@ def get_mouse_brain_sagittal_data(path=None, section=None, cut=None):
         ">>> ERROR: Ambiguous appointment!"
 
     if (not path):
-        path = os.path.join('mouse_brain_sagittal/', 'section_'+str(section), cut)
+        path = os.path.join('spatial_dataset', 'mouse_brain_sagittal', 'section_'+str(section), cut)
     adata = build_adata(spot_scale=3., 
                         root_path=path, 
                         name='mouse_brain_sagittal')
@@ -64,7 +77,7 @@ def get_mouse_brain_sagittal_data(path=None, section=None, cut=None):
 
 def get_mouse_kidney_coronal_data(path=None):
     if (not path):
-        path = 'mouse_kidney_coronal/'
+        path = 'spatial_dataset/mouse_kidney_coronal/'
     adata = build_adata(spot_scale=1., 
                         root_path=path, 
                         name='mouse_kidney_coronal')
@@ -74,7 +87,7 @@ def get_mouse_kidney_coronal_data(path=None):
 
 def get_mouse_olfactory_bulb_ST_data(id, path=None):
     if (not path):
-        path = 'mouse_olfactory_bulb_ST/'
+        path = 'spatial_dataset/mouse_olfactory_bulb_ST/'
     adata = build_adata(spot_scale=10., 
                         root_path=os.path.join(path, 'slice_{}'.format(id)), 
                         name='mouse_olfactory_bulb_ST_slice_{}'.format(id))
@@ -84,7 +97,7 @@ def get_mouse_olfactory_bulb_ST_data(id, path=None):
 
 def get_mouse_olfactory_bulb_Stereo_seq_data(path=None):
     if (not path):
-        path = 'mouse_olfactory_bulb_Stereo-seq/'
+        path = 'spatial_dataset/mouse_olfactory_bulb_Stereo-seq/'
     adata = build_adata(spot_scale=2., 
                         root_path=path, 
                         name='mouse_olfactory_bulb_Stereo-seq')
@@ -94,7 +107,7 @@ def get_mouse_olfactory_bulb_Stereo_seq_data(path=None):
 
 def get_mouse_somatosensory_cortex_data(path=None):
     if (not path):
-        path = 'mouse_somatosensory_cortex/'
+        path = 'spatial_dataset/mouse_somatosensory_cortex/'
     adata = build_adata(spot_scale=2., 
                         root_path=path, 
                         name='mouse_somatosensory_cortex')
