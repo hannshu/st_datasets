8ae106a89085c7e154fea18d3995e507275421ca
hannshu <hanshu.npu@gmail.com>
2023-11-08 22:48:31 +0800

    add visualize graph, some multi-slices methods

--- utils/plotting.py
+++ utils/plotting.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import scanpy as sc
+import squidpy as sq
+import numpy as np
+from scipy.sparse import coo_matrix
 
 
 def show_distrib_map(adata, title='data_distribution_map', format='hist', **args):
@@ -21,3 +25,29 @@ def show_distrib_map(adata, title='data_distribution_map', format='hist', **args
         sns.histplot(val, **args)
         plt.title(title)
     plt.savefig(title+'.png', dpi=600)
+
+
+def visualize_graph(adata: sc.AnnData, edges, save_path=None, color_label=None):
+    without_ring = np.array([[edge[0], edge[1]] for edge in edges if (edge[0] != edge[1])]).T
+    if ((0, ) != without_ring.shape):
+        adata.obsp['visualize'] = coo_matrix(
+            ([1]*without_ring.shape[1], (without_ring)), 
+            shape=(adata.X.shape[0], adata.X.shape[0]), 
+            dtype=np.int32
+        )
+        
+        if (None == color_label):
+            adata.obs['default'] = ['1'] * adata.X.shape[0]
+            color_label = ['default']
+
+        sq.pl.spatial_scatter(
+            adata, connectivity_key="visualize", img=False, color=color_label, legend_loc=None,
+            edges_width=0.25, title='', axis_label=['', ''], edges_color='black'
+        )
+        if (save_path):
+            plt.savefig(save_path, dpi=1600)
+
+        del adata.obsp['visualize']
+    else:
+        print(">>> WARNING: No edge to draw!")
+
--- utils/preprocess.py
+++ utils/preprocess.py
@@ -3,24 +3,30 @@ import scanpy as sc
 import numpy as np
 import networkx as nx
 from sklearn.neighbors import NearestNeighbors
+import time
 
 
-def build_graph(adata: sc.AnnData, radius=None, knears=None):
-    coor = pd.DataFrame(adata.obsm['spatial'])
-    coor.index = adata.obs.index
-    coor.columns = ['row', 'col']
+def build_graph(adata: sc.AnnData, radius=None, knears=None, distance_metrics='l2', use_repo='spatial', matrix=None):
+    start = time.time()
+
+    if (isinstance(matrix, np.ndarray)):
+        coor = pd.DataFrame(matrix)
+    elif ('X' == use_repo):
+        coor = pd.DataFrame(adata.X.todense())
+    else:
+        coor = pd.DataFrame(adata.obsm[use_repo])
+        coor.index = adata.obs.index
+        coor.columns = ['row', 'col']
 
     if (radius):
-        nbrs = NearestNeighbors(radius=radius).fit(coor)
+        nbrs = NearestNeighbors(radius=radius, metric=distance_metrics).fit(coor)
         _, indices = nbrs.radius_neighbors(coor, return_distance=True)
     else:
-        nbrs = NearestNeighbors(n_neighbors=knears+1).fit(coor)
+        nbrs = NearestNeighbors(n_neighbors=knears+1, metric=distance_metrics).fit(coor)
         _, indices = nbrs.kneighbors(coor)
 
-    edge_list = []
-    for i in range(len(indices)):
-        for j in range(len(indices[i])):
-            edge_list.append([i, indices[i][j]])
+    edge_list = np.array([[i, j] for i, sublist in enumerate(indices) for j in sublist])
+    print(f">>> INFO: Generate {edge_list.shape[0]} edges, {edge_list.shape[0] / adata.shape[0]:.3f} edges per spot.({time.time() - start:.3f}s)")
 
     return edge_list
 
@@ -36,10 +42,31 @@ def convert_edge_to_adj(edge_list, spot_num=None, dense=True):
     
     if (dense):
         return nx.adjacency_matrix(g).todense()
-    else :
+    else:
         return nx.adjacency_matrix(g)
 
 
 def convert_adj_to_edge(adj: np.array):
     return np.nonzero(adj)
 
+
+def concat_adjacency_matrix(adata_list, edge_list, return_type=None):
+    edges = edge_list[0]
+    spot_count = adata_list[0].X.shape[0]
+
+    for i in range(1, len(adata_list)):
+        edge_list[i] = spot_count + edge_list[i]
+        edges = np.vstack((edges, edge_list[i]))
+        spot_count += adata_list[i].X.shape[0]
+
+    if ('adj' == return_type):
+        return convert_edge_to_adj(edges.T)
+    else:
+        return edges.T
+    
+
+def conv_to_one_hot(X, n):
+    X = np.array(X).astype(int)
+    refer = np.eye(n)
+    X_one_hot = refer[X]
+    return X_one_hot
